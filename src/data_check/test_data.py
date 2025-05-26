c08d727212139c787a86f9caa080b96fb216c9c5
datalytix-777 <user1@Computer.>
2025-05-26 15:32:09 -0400

    Corrected comma error in run.py, added tests for row_count & price_range

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,12 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+#confirms dataset is reasonable size
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+#confirms price is in desired range
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all()
+
